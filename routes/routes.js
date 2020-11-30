4729233a1945c7ba89d92a599c051a8ff770f51a
Logan Coates <coates.logan1125@gmail.com>
2020-11-30 13:46:36 -0700

    fixed route js bug

--- routes/routes.js
+++ routes/routes.js
@@ -3,19 +3,13 @@ const path = require('path');
 
 module.exports = app => {
 
-    // Setup notes variable
     fs.readFile("db/db.json","utf8", (err, data) => {
 
         if (err) throw err;
 
         var notes = JSON.parse(data);
 
-        // API ROUTES
-        // ========================================================
-    
-        // Setup the /api/notes get route
         app.get("/api/notes", function(req, res) {
-            // Read the db.json file and return all saved notes as JSON.
             res.json(notes);
         });
 
