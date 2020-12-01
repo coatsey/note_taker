5feab302dc546e091c8597392f86ec697a75fbff
Logan Coates <coates.logan1125@gmail.com>
2020-11-30 22:15:45 -0700

    Fixed bugs

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 
 const express = require("express");
-const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
 
-require('./routes/routes')(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.use(express.static("public"));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
-});  
+});
